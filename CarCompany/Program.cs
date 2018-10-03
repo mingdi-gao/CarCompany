20549656fe614458f535c506fa3c440a6e237820
Gao <gaomingdi@gmail.com>
2018-10-03 00:57:49 -0400

    first commit

--- CarCompany/Program.cs
+++ CarCompany/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CarDAL;
+using CarModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +12,127 @@ namespace CarCompany
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Loading Data......");
+            CarDal carManager = new CarDal();
+            carManager.loadData();
+            Console.WriteLine("Sample data loaded succesfully!");
+          
+            while (true)
+            {
+                List<Car> results = new List<Car>();
+                Car result = new Car();
+                decimal distance = 0;
+                
+                ShowMenu();
+                string command = Console.ReadLine();
+                switch (command.ToUpper())
+                {
+                    case "1":
+                        results = carManager.getNewestVehiclesInOrder();
+                        displayResults(results);
+                        break;
+                    case "2":
+                        results = carManager.getalphabetizedr();
+                        displayResults(results);
+                        break;
+                    case "3":
+                        results = carManager.getOrderByPrice();
+                        displayResults(results);
+                        break;
+                    case "4":
+                        result = carManager.getBestValue();
+                        displaySingleResult(result);
+                        break;
+                    case "5":
+                        Console.WriteLine("Enter the distance");
+                        distance = Convert.ToInt32(Console.ReadLine());
+                        var fuleConsumptions = carManager.getFuleConsumption(distance);
+                        displayFuelConsumption(fuleConsumptions);
+                        break;
+                    case "6":
+                        result = carManager.getRandomCar();
+                        displaySingleResult(result);
+                        break;
+                    case "7":
+                        Console.WriteLine("Enter the year(2016 or 2017)");
+                        int year = Convert.ToInt32(Console.ReadLine());
+                        var avgMPG = carManager.averageMPGByYear(year);
+                        displayAvgMPG(avgMPG, year);
+                        break;
+                    case "Q":
+                        Environment.Exit(1);
+                        break;
+                    default:
+                        Console.WriteLine("Invalid Command");
+                        break;
+                }
+            }
+
+        }
+
+        public static void ShowMenu()
+        {
+
+            Console.WriteLine();
+
+            Console.WriteLine("1 - Calculate the newest vehicles in order");
+            Console.WriteLine("2 - Calculate an alphabetized List of vehicles");
+            Console.WriteLine("3 - Calculate an ordered List of Vehicles by Price");
+            Console.WriteLine("4 - Calculate the best value");
+            Console.WriteLine("5 - Calculate fuel consumption for a given distance");
+            Console.WriteLine("6 - Return a random car from the list");
+            Console.WriteLine("7 - Return average MPG by year");
+            Console.WriteLine("Q - Quit");
+            Console.WriteLine();
+            Console.WriteLine("Enter your command(1~7) or Press 'Q' to exit:");
+        }
+
+        public static void displayResults(List<Car> resultCollection)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Make   Model   Color    Year    Price   TCCRating   HwyMPG");
+            foreach (var c in resultCollection)
+            {
+                Console.WriteLine(c.Make + "   " + 
+                    c.Model + "   " + 
+                    c.Color + "   " + 
+                    c.Year + "   " + 
+                    c.Price + "    " + 
+                    c.TCCRating + "    " + 
+                    c.HwyMPG);
+            }
+        }
+
+        public static void displaySingleResult(Car c)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Make   Model   Color    Year    Price   TCCRating   HwyMPG");
+            Console.WriteLine(c.Make + "   " +
+                    c.Model + "   " +
+                    c.Color + "   " +
+                    c.Year + "   " +
+                    c.Price + "    " +
+                    c.TCCRating + "    " +
+                    c.HwyMPG);
+
+        }
+
+        public static void displayFuelConsumption(Dictionary<string, decimal> carDic)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Model  FuelConsumption");
+            foreach(var c in carDic)
+            {
+                Console.WriteLine(c.Key + "  " + c.Value);
+            }
+
+        }
+
+        public static void displayAvgMPG(decimal avgMPG, int year)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Year    avgMPG");
+            Console.WriteLine(year + " " + avgMPG);
         }
     }
 }
---
+++ CarDAL/CarDal.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CarModel;
+
+
+namespace CarDAL
+{
+    public class CarDal
+    {
+        private List<Car> carList = new List<Car>();
+
+        public void loadData()
+        {
+            var rawdata = System.IO.File.ReadAllText("SampleData.json");
+            carList = Newtonsoft.Json.JsonConvert.DeserializeObject<List<Car>>(rawdata);
+        }
+
+        public List<Car> getNewestVehiclesInOrder()
+        {
+            return carList.OrderByDescending(l => l.Year).ToList();
+        }
+        public List<Car> getalphabetizedr()
+        {
+            return carList.OrderBy(l => l.Model).ToList();
+        }
+        public List<Car> getOrderByPrice()
+        {
+            return carList.OrderBy(l => l.Price).ToList();
+        }
+        public Car getBestValue()
+        {
+            return carList.OrderByDescending(l => l.TCCRating).FirstOrDefault();
+        }
+        public Dictionary<string, decimal> getFuleConsumption(decimal distance)
+        {
+            Dictionary<string, decimal> dict = new Dictionary<string, decimal>();
+            carList.ForEach(l => dict.Add(l.Model, distance / l.HwyMPG));
+            return dict;
+        }
+        public Car getRandomCar()
+        {
+            Random rd = new Random();
+            int index = rd.Next(0, 5);
+            return carList[index];
+        }
+        public decimal averageMPGByYear(int year)
+        {
+            return carList.Where(l => l.Year == year).Average(l => l.HwyMPG);
+        }
+    }
+}
+
